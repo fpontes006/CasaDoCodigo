8e180e1a87dfd54a70a57b3d5cbc6019e6dfa031
fh191 <fh191@t-systems.com.br>
2022-10-07 15:09:41 -0300

    Add project files.

---
+++ Aulas/Aula1/CasaDoCodigo/DataService.cs
@@ -0,0 +1,39 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    class DataService : IDataService
+    {
+        private readonly ApplicationContext contexto;
+        private readonly IProdutoRepository produtoRepository;
+
+        public DataService(ApplicationContext contexto,IProdutoRepository produtoRepository)
+        {
+            this.contexto = contexto;
+            this.produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDb()
+        {
+            contexto.Database.Migrate();
+            List<Livro> livros = getLivros();
+            produtoRepository.Savelivros(livros);
+        }
+
+        
+
+        private static List<Livro> getLivros()
+        {
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+
+    
+}
